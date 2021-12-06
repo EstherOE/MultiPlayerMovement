096b63ec65ebeb46e2375496e2983b6007b3b365
Esther O Epelle <epelleoesther@gmail.com>
2021-12-06 15:37:46 +0100

    It is a multiPlayer game

---
+++ Assets/Mirror/Examples/Benchmark/Scripts/PlayerMovement.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+namespace Mirror.Examples.Benchmark
+{
+    public class PlayerMovement : NetworkBehaviour
+    {
+        public float speed = 5;
+
+        void Update()
+        {
+           
+
+            float h = Input.GetAxis("Horizontal");
+            float v = Input.GetAxis("Vertical");
+
+            Vector3 dir = new Vector3(h, 0, v);
+            transform.position += dir.normalized * (Time.deltaTime * speed);
+            Debug.Log(dir);
+        }
+    }
+}
